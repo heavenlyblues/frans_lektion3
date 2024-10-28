c02c1ef6eead05bbd32b9b4a22bb3d254f25c087
Nicholas Stevens <nichstev@gmail.com>
2024-10-28 19:26:37 +0100

    Added a few alternative versions

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,5 @@
-for n in range(1, 100):
+# Using if elif statements
+for n in range(1, 101):
     if n % 3 != 0 and n % 5 != 0:
         print(f"{n}")
     elif n % 3 == 0 and n % 5 == 0:
@@ -7,4 +8,22 @@ for n in range(1, 100):
         print("Fizz")
     elif n % 5 == 0:
         print("Buzz")
-              
+
+# Using string concantenation              
+for n in range(1, 101):
+    output = ""
+    if n % 3 == 0:
+        output += "Fizz"
+    if n % 5 == 0:
+        output += "Buzz"
+    print(output or n)
+
+
+for n in range(1, 101):
+    result = (
+        "FizzBuzz" if n % 15 == 0 else
+        "Fizz" if n % 3 == 0 else
+        "Buzz" if n % 5 == 0 else
+        str(n)
+    )
+    print(result)
